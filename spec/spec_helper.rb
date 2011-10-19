cc22d90ebfd384681e30261c385daab1f09b5881
Jian Lin <linjian815@gmail.com>
2011-10-19 17:16:10 +0800

    fix for spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,6 @@
 require 'rubygems'
 require 'spork'
 
-Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
-
 def rspec_configure
   RSpec.configure do |config|
     config.include UserSpecHelper
@@ -23,21 +21,24 @@ def rspec_configure
   end
 end
 
+def load_supports
+  # Requires supporting ruby files with custom matchers and macros, etc,
+  # in spec/support/ and its subdirectories.
+  Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+end
+
 Spork.prefork do
   # This file is copied to spec/ when you run 'rails generate rspec:install'
   ENV["RAILS_ENV"] ||= 'test'
   require File.expand_path("../../config/environment", __FILE__)
   require 'rspec/rails'
 
+  load_supports
   rspec_configure
 end
 
 Spork.each_run do
   require 'config/routes'
-
-  # Requires supporting ruby files with custom matchers and macros, etc,
-  # in spec/support/ and its subdirectories.
-  Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
-
+  load_supports
   require 'factory_girl_rails'
 end
